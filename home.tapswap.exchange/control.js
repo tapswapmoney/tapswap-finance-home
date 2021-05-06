93703f4522512cd14297dcb187ae763bb3828986
tapswap <tapswapmoneydex@gmail.com>
2021-05-06 18:36:48 +0800

    init commit

---
+++ home.tapswap.exchange/control.js
@@ -0,0 +1,27 @@
+const mainNav = document.getElementById("mainnav");
+const logoColumn = document.getElementById("logo-column");
+const navBtn = document.querySelector(".burger-button");
+
+var mainNavOpen = false;
+
+navBtn.addEventListener("click", toggleNav);
+mainNav.addEventListener("load", toggleNav);
+
+window.addEventListener("load", () => {
+  mainNav.style.left = "-14vw";
+  mainNavOpen = false;
+  console.log(mainNavOpen);
+});
+
+function toggleNav() {
+  if (mainNavOpen) {
+    mainNav.style.left = "-14vw";
+    mainNavOpen = false;
+  } else {
+    /**
+     * if close --> open
+     */
+    mainNav.style.left = "0vw";
+    mainNavOpen = true;
+  }
+}
