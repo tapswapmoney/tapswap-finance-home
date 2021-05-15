de0091d3c3cfb0a9595a48807682dd85c543d95d
tapswap <tapswapmoneydex@gmail.com>
2021-05-15 13:52:58 +0800

    responsiveness

--- control.js
+++ control.js
@@ -1,26 +1,34 @@
 const mainNav = document.getElementById("mainnav");
 const logoColumn = document.getElementById("logo-column");
 const navBtn = document.querySelector(".burger-button");
+const navBtnChild = document.querySelector("#navigation-button");
 
 var mainNavOpen = false;
 
 navBtn.addEventListener("click", toggleNav);
-mainNav.addEventListener("load", toggleNav);
 
 window.addEventListener("load", () => {
-  mainNav.style.left = "-13vw";
+  mainNav.style.left = `-250px`;
   mainNavOpen = false;
-  console.log(mainNavOpen);
+});
+
+window.addEventListener("resize", () => {
+  mainNav.style.left = `-250px`;
+  mainNavOpen = false;
 });
 
 function toggleNav() {
   if (mainNavOpen) {
-    mainNav.style.left = "-13vw";
+    mainNav.style.left = "-250px";
     mainNavOpen = false;
+    navBtnChild.classList.add("ti-align-right");
+    navBtnChild.classList.remove("ti-close");
   } else {
     /**
      * if close --> open
      */
+    navBtnChild.classList.remove("ti-align-right");
+    navBtnChild.classList.add("ti-close");
     mainNav.style.left = "0vw";
     mainNavOpen = true;
   }
